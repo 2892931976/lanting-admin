bea74aabedc52204861b8181b04353295828ae34
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-25 10:20:18 +0000

    Deploy Vultur/lanting-admin to github.com/Vultur/lanting-admin.git:gh-pages

---
+++ 27.async.js
@@ -0,0 +1,1 @@
+webpackJsonp([27],{189:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var i=t(94),u=t.n(i),l=t(0),r=(t.n(l),t(132)),a=(t.n(r),t(529));n.default=function(){return u()(a.a,{type:"404",style:{minHeight:500,height:"80%"},linkElement:r.Link})}}});

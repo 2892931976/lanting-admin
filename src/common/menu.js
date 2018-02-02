b861c439bc2c4f418bbb141ae8e0e4178738a759
Vultur <wall-e@live.cn>
2018-02-02 15:43:24 +0800

    他老婆辛辛苦苦提交的代码呀呀呀呀

--- src/common/menu.js
+++ src/common/menu.js
@@ -1,123 +1,105 @@
 import { isUrl } from '../utils/utils';
 
 const menuData = [{
-  name: 'dashboard',
+  name: '酒店概况',
   icon: 'dashboard',
   path: 'dashboard',
   children: [{
-    name: '分析页',
+    name: '酒店简介',
+    icon: 'dashboard',
     path: 'analysis',
   }, {
-    name: '监控页',
+    name: '交通指引',
     path: 'monitor',
   }, {
-    name: '工作台',
+    name: '招聘信息',
     path: 'workplace',
-    // hideInMenu: true,
+  }, {
+    name: '友情链接',
+    path: 'workplace',
+  }, {
+    name: '联系我们',
+    path: 'workplace',
   }],
 }, {
-  name: '表单页',
+  name: '客房管理',
   icon: 'form',
   path: 'form',
   children: [{
-    name: '基础表单',
+    name: '客房设施',
     path: 'basic-form',
   }, {
-    name: '分步表单',
+    name: '客房管理',
     path: 'step-form',
-  }, {
-    name: '高级表单',
-    authority: 'admin',
-    path: 'advanced-form',
   }],
 }, {
-  name: '列表页',
+  name: '会议宴会',
   icon: 'table',
   path: 'list',
   children: [{
-    name: '查询表格',
+    name: '会议场地',
     path: 'table-list',
   }, {
-    name: '标准列表',
+    name: '宴会服务',
     path: 'basic-list',
-  }, {
-    name: '卡片列表',
-    path: 'card-list',
-  }, {
-    name: '搜索列表',
-    path: 'search',
-    children: [{
-      name: '搜索列表（文章）',
-      path: 'articles',
-    }, {
-      name: '搜索列表（项目）',
-      path: 'projects',
-    }, {
-      name: '搜索列表（应用）',
-      path: 'applications',
-    }],
   }],
 }, {
-  name: '详情页',
+  name: '休闲娱乐',
   icon: 'profile',
   path: 'profile',
-  children: [{
-    name: '基础详情页',
-    path: 'basic',
-  }, {
-    name: '高级详情页',
-    path: 'advanced',
-    authority: 'admin',
-  }],
 }, {
-  name: '结果页',
+  name: '餐饮美食',
   icon: 'check-circle-o',
   path: 'result',
   children: [{
-    name: '成功',
+    name: '酒店餐厅',
     path: 'success',
   }, {
-    name: '失败',
+    name: '菜肴菜品',
     path: 'fail',
   }],
 }, {
-  name: '异常页',
+  name: '城市指南',
   icon: 'warning',
   path: 'exception',
   children: [{
-    name: '403',
+    name: '城市介绍',
     path: '403',
   }, {
-    name: '404',
+    name: '名胜景点',
     path: '404',
   }, {
-    name: '500',
+    name: '特色美食',
     path: '500',
   }, {
-    name: '触发异常',
+    name: '购物中心',
+    path: 'trigger',
+    hideInMenu: true,
+  }, {
+    name: '休闲娱乐',
     path: 'trigger',
     hideInMenu: true,
   }],
 }, {
-  name: '账户',
+  name: '图片轮播',
   icon: 'user',
   path: 'user',
   authority: 'guest',
-  children: [{
-    name: '登录',
-    path: 'login',
-  }, {
-    name: '注册',
-    path: 'register',
-  }, {
-    name: '注册结果',
-    path: 'register-result',
-  }],
 }, {
-  name: '使用文档',
-  icon: 'book',
-  path: 'http://pro.ant.design/docs/getting-started',
-  target: '_blank',
+  name: '搜索优化',
+  icon: 'warning',
+  path: 'exception',
+}, {
+  name: '系统设置',
+  icon: 'warning',
+  path: 'exception',
+  children: [{
+    name: '用户管理',
+    path: '403',
+  }, {
+    name: '系统日志',
+    path: '404',
+  }],
 }];
 
 function formatter(data, parentPath = '', parentAuthority) {
